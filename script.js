5687259bf061be55ceafcd15d5feb96bae9b721a
Sumit kumar <xperia7048@gmail.com>
2024-02-28 22:47:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+    let box = document.getElementById("box");
+    console.log(box);
+    box.style.backgroundColor = "yellow";
+    box.style.margin = "20px";
+    box.style.padding = "10px";
+    box.style.height = "200px";
+    box.style.width =  "300px";
+    box.style.fontSize = "18px";
+    box.style.fontWeight = "bold";
+    box.style.color = "blue";
